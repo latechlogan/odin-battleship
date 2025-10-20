1810cca2adf9f51a0b6dc478dea17586685c74e0
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-20 15:57:12 -0500

    feat: add attack & end game handling to gameboard

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,11 +1,19 @@
-function parseCoordinate(array) {
-  return array.map((coord) => {
-    const letter = coord[0];
-    const number = parseInt(coord.slice(1));
-    const row = letter.charCodeAt(0) - 65; // 'A' = 65 in ASCII
+function parseCoordinates(input) {
+  const parse = function (input) {
+    const letter = input[0];
+    const number = parseInt(input.slice(1));
+    const row = letter.charCodeAt(0) - 65; //'A' = 65 in ASCII
     const col = number - 1;
     return [row, col];
-  });
+  };
+
+  if (typeof input === "string") {
+    return parse(input);
+  } else {
+    return input.map((coord) => {
+      return parse(coord);
+    });
+  }
 }
 
 function formatCoordinate(row, col) {
@@ -23,8 +31,30 @@ class Gameboard {
   placeShip(ship, coordinates) {
     this.ships.push({
       ship: ship,
-      coordinates: parseCoordinate(coordinates),
+      coordinates: parseCoordinates(coordinates),
     });
+  }
+
+  receiveAttack(coord) {
+    let attack = parseCoordinates(coord);
+    let shipAttacked = this.ships.find((obj) =>
+      obj.coordinates.some(
+        (coord) => coord[0] === attack[0] && coord[1] === attack[1]
+      )
+    );
+
+    if (shipAttacked) {
+      shipAttacked.ship.hit();
+      if (shipAttacked.ship.isSunk()) {
+        if (this.fleetIsSunk()) return { gameOver: true };
+      }
+    } else {
+      this.missedAttacks.push(attack);
+    }
+  }
+
+  fleetIsSunk() {
+    return this.ships.every((obj) => obj.ship.isSunk());
   }
 }
 
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -8,6 +8,7 @@ describe("gameboard", () => {
     testGameboard = new Gameboard();
   });
 
+  //ship placement
   it("should place ship at given coordinates", () => {
     const destroyer = new Ship(2);
     testGameboard.placeShip(destroyer, ["A1", "A2"]);
@@ -26,5 +27,37 @@ describe("gameboard", () => {
     testGameboard.placeShip(destroyer, ["A1", "A2"]);
     testGameboard.placeShip(submarine, ["B1", "B2", "B3"]);
     expect(testGameboard.ships.length).toBe(2);
+  });
+
+  //attack handling
+  it("tracks attacks that hit", () => {
+    const destroyer = new Ship(2);
+    testGameboard.placeShip(destroyer, ["A1", "A2"]);
+    testGameboard.receiveAttack("A1");
+    expect(destroyer.hits).toBe(1);
+  });
+
+  it("tracks attacks that miss", () => {
+    const destroyer = new Ship(2);
+    testGameboard.placeShip(destroyer, ["A1", "A2"]);
+    testGameboard.receiveAttack("A3");
+    expect(testGameboard.missedAttacks[0]).toEqual([0, 2]);
+  });
+
+  it("tracks multiple missed attacks", () => {
+    const destroyer = new Ship(2);
+    testGameboard.placeShip(destroyer, ["A1", "A2"]);
+    testGameboard.receiveAttack("A3");
+    testGameboard.receiveAttack("A4");
+    expect(testGameboard.missedAttacks.length).toBe(2);
+  });
+
+  //end game handling
+  it("tracks when an entire fleet is sunk", () => {
+    const destroyer = new Ship(2);
+    testGameboard.placeShip(destroyer, ["A1", "A2"]);
+    testGameboard.receiveAttack("A1");
+    const result = testGameboard.receiveAttack("A2");
+    expect(result).toEqual({ gameOver: true });
   });
 });
