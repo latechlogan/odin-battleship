4253b87eaa573f06374e255eabb83a016bf482b9
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-20 21:25:04 -0500

    refactor: move helper functions into helper module

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,26 +1,4 @@
-function parseCoordinates(input) {
-  const parse = function (input) {
-    const letter = input[0];
-    const number = parseInt(input.slice(1));
-    const row = letter.charCodeAt(0) - 65; //'A' = 65 in ASCII
-    const col = number - 1;
-    return [row, col];
-  };
-
-  if (typeof input === "string") {
-    return parse(input);
-  } else {
-    return input.map((coord) => {
-      return parse(coord);
-    });
-  }
-}
-
-function formatCoordinate(row, col) {
-  const letter = String.fromCharCode(65 + row);
-  const number = col + 1;
-  return `${letter}${number}`;
-}
+import { parseCoordinates } from "./helpers";
 
 class Gameboard {
   constructor() {
