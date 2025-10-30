083c18c011882314589982cff6da459de7330075
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-10-29 22:38:33 -0500

    chore: remove unused code from game.js

--- src/modules/game.js
+++ src/modules/game.js
@@ -1,6 +1,5 @@
 const HumanPlayer = require("./humanPlayer");
 const ComputerPlayer = require("./computerPlayer");
-const Ship = require("./ship");
 
 class Game {
   #isPlayerTurn = true;
@@ -35,39 +34,6 @@ class Game {
     if (!this.isGameOver) return null;
 
     return this.player.gameboard.fleetSunk() ? "Computer" : "Player";
-  }
-
-  reset() {
-    this.player = new HumanPlayer();
-    // hardcode player ships for development/testing
-    this.player.gameboard.placeShip(new Ship(5), [
-      "A1",
-      "A2",
-      "A3",
-      "A4",
-      "A5",
-    ]);
-    this.player.gameboard.placeShip(new Ship(4), ["C1", "C2", "C3", "C4"]);
-    this.player.gameboard.placeShip(new Ship(3), ["E1", "E2", "E3"]);
-    this.player.gameboard.placeShip(new Ship(3), ["G1", "G2", "G3"]);
-    this.player.gameboard.placeShip(new Ship(2), ["I1", "I2"]);
-
-    this.computer = new ComputerPlayer();
-    // hardcode computer ships for development/testing
-    this.computer.gameboard.placeShip(new Ship(5), [
-      "B2",
-      "C2",
-      "D2",
-      "E2",
-      "F2",
-    ]);
-    this.computer.gameboard.placeShip(new Ship(4), ["B4", "C4", "D4", "E4"]);
-    this.computer.gameboard.placeShip(new Ship(3), ["B6", "C6", "D6"]);
-    this.computer.gameboard.placeShip(new Ship(3), ["H1", "H2", "H3"]);
-    this.computer.gameboard.placeShip(new Ship(2), ["J9", "J10"]);
-
-    this.#isPlayerTurn = true;
-    this.#lastResult = null;
   }
 
   playTurn(coord) {
@@ -106,7 +72,5 @@ class Game {
     return result;
   }
 }
-// Manage turn flow (human attacks → computer attacks)
-// Check win conditions
 
 module.exports = Game;
